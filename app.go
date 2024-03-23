95b06a114c487ccae8f7219fc7a51fb3364739ce
Chadi00 <elkhiatichadi@outlook.fr>
2024-03-23 03:06:51 -0400

    setup port

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"milio/db"
 	"milio/routes"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,5 +16,10 @@ func main() {
 
 	routes.GenerateRoutes(server)
 
-	server.Run(":8080") //localhost:8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	server.Run(":" + port)
 }
